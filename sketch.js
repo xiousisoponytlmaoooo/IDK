8ce28b31f774b72af6e4e053991c0b4ccdd99d82
xiousisoponytlmaoooo <shabbirmukuljha231@gmail.com>
2021-10-01 17:47:05 +0530

    your message here

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var bg, backgroundImg;
+
+function preload() {
+  backgroundImg = loadImage("images/bg.jpg");
+ 
+}
+
+function setup() {
+  createCanvas(1000, 600);
+  bg = createSprite(580,300);
+ 
+}
+
+function draw() {
+  
+ 
+    
+    drawSprites();
+   
+}
+
